49966ae1f947b18cd607a8e8a5a568d6ed6bb31c
Neo Alienson <neo@orz.hk>
2017-12-08 21:12:10 +0800

    enable js_concat and all_minifier during generation only.

--- generate.sh
+++ generate.sh
@@ -2,7 +2,13 @@
 rm -rf public
 
 cd hexo-blog
+# enable all_minifier and js_concator
+sed -i.bak "s/all_minifier: false/all_minifier: true/" _config.yml
+sed -i.bak "s/js_concator: false/js_concator: true/" _config.yml
 hexo generate
+# disable all_minifier and js_concator
+sed -i.bak "s/all_minifier: true/all_minifier: false/" _config.yml
+sed -i.bak "s/js_concator: true/js_concator: false/" _config.yml
 
 mv public ..
 cd ..
