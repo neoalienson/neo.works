5b8ac329f1e18f0ca3af790958f42bc15fe8dca4
Neo Aleinson <neo@padblish.com>
2019-12-06 02:28:14 +0800

    upate deploy script

--- deploy-hosting.sh
+++ deploy-hosting.sh
@@ -3,19 +3,9 @@
 # git clone git@github.com:neoalienson/neo.works.git .deploy
 cd .deploy/hexo-blog
 
-rm themes/hexo-theme-hueman/_config.yml
-
 git reset --hard
 git pull
 git submodule update --init --recursive
-pwd
-pushd themes/hexo-theme-hueman
-git reset --hard
-git apply ../../../hexo-blog-patch/0002-Add-terms-and-condition-to-footer.patch
-git apply ../../../hexo-blog-patch/0001-Add-title-above-subtitle.patch
-popd
-
-cp themes/_config.yml themes/hexo-theme-hueman
 
 npm install
 
